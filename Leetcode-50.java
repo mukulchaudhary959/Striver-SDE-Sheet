6af13635866aedff7bfbc9a2775bcb8f8e72d133
Mukul Chaudhary <119926348+mukulchaudhary959@users.noreply.github.com>
2023-10-31 21:29:53 +0530

    Create Leetcode-50.java

---
+++ Leetcode-50.java
@@ -0,0 +1,19 @@
+class Solution {
+    public double myPow(double x, long n) {
+        if(x==0){
+            return 0;
+        }
+        if(x==1){
+            return 1;
+        }
+      
+       
+          if(n==0){
+              return 1;
+          }
+             if(n<0)
+            return myPow(1/x,Math.abs(n));
+
+        return (n%2==0 ? myPow(x*x,n/2) : x*myPow(x*x,n/2));
+    }
+}
