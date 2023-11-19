ae43af39d51364f8476a1d99a8714b20f233a754
Mukul Chaudhary <119926348+mukulchaudhary959@users.noreply.github.com>
2023-11-19 21:38:46 +0530

    Create Leetcode-15.java

---
+++ Leetcode-15.java
@@ -0,0 +1,26 @@
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        HashSet<List<Integer>> set = new HashSet<List<Integer>>();
+        if(nums.length==0){
+            return new ArrayList<>();
+        }
+        Arrays.sort(nums);
+        for(int i=0;i<nums.length-2;i++){
+             int j=i+1;
+             int k = nums.length-1;
+             while(j<k){
+                 int sum = nums[i]+nums[j]+nums[k];
+                 if(sum==0){
+                     set.add(Arrays.asList(nums[i],nums[j++],nums[k--]));
+                 }
+                 if(sum>0){
+                     k--;
+                 }
+                 else if(sum<0){
+                     j++;
+                 }
+             }
+        }
+        return new ArrayList<>(set);
+    }
+}
