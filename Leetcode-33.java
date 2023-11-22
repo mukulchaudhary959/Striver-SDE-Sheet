b016ea095cb5769a89b38e970f52d634a32b30fa
Mukul Chaudhary <119926348+mukulchaudhary959@users.noreply.github.com>
2023-11-22 17:24:10 +0530

    Create Leetcode-33.java

---
+++ Leetcode-33.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int s =0;
+        int e = nums.length-1;
+        while(s<=e){
+            int m = s+(e-s)/2;
+            if(nums[m]==target){
+                return m;
+            }
+            else{
+            if(nums[m]>=nums[s]){
+                if(nums[m]>=target && nums[s]<=target){
+                    e=m-1;
+                }
+                else{
+                    s=m+1;
+                }
+            }
+            else{
+                if(nums[m]<=target && nums[e]>=target){
+                    e=m-1;
+                }
+                else{
+                    s=m+1;
+                }
+            }
+            }
+        }
+            return -1;
+        
+    }
+}
